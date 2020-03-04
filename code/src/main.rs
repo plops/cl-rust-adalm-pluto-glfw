add29e78ba574ec00d8809ba930ea532eb915a00
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-03-04 07:36:47 +0100

    start combining everything into one file

--- code/src/main.rs
+++ code/src/main.rs
@@ -8,15 +8,24 @@ extern crate crossbeam_channel;
 extern crate fftw;
 extern crate imgui_glfw_rs;
 extern crate industrial_io as iio;
-mod fft_scaler;
-mod iio_reader;
+use crossbeam_channel::bounded;
+use fftw;
+use fftw::plan::C2CPlan;
 use iio_reader::SendComplex;
 use imgui_glfw_rs::glfw::{Action, Context, Key};
 use imgui_glfw_rs::ImguiGLFW;
 use std::ffi::CString;
+use std::io;
 use std::os::raw::c_void;
+use std::sync::{Arc, Mutex};
+use std::thread::spawn;
+pub struct SendComplex {
+    pub timestamp: DateTime<Utc>,
+    pub ptr: fftw::array::AlignedVec<num_complex::Complex<f64>>,
+}
+unsafe impl Send for SendComplex {}
 fn main() {
-    let mut fftin = std::sync::Arc::new(std::sync::Mutex::new([
+    let mut fftin = [
         std::sync::Arc::new(std::sync::Mutex::new(SendComplex {
             timestamp: Utc::now(),
             ptr: fftw::array::AlignedVec::new(512),
@@ -29,8 +38,8 @@ fn main() {
             timestamp: Utc::now(),
             ptr: fftw::array::AlignedVec::new(512),
         })),
-    ]));
-    let mut fftout = std::sync::Arc::new(std::sync::Mutex::new([
+    ];
+    let mut fftout = [
         std::sync::Arc::new(std::sync::Mutex::new(SendComplex {
             timestamp: Utc::now(),
             ptr: fftw::array::AlignedVec::new(512),
@@ -43,12 +52,206 @@ fn main() {
             timestamp: Utc::now(),
             ptr: fftw::array::AlignedVec::new(512),
         })),
-    ]));
+    ];
+    let core_ids = core_affinity::get_core_ids().unwrap();
+    let b = std::thread::Builder::new().name("pluto_reader".into());
+    let reader_thread = b.spawn(move || {
+        core_affinity::set_for_current(core_affinity::CoreId { id: 0 });
+        let ctx = iio::Context::create_network("192.168.2.1").unwrap_or_else(|err_| {
+            {
+                println!(
+                    "{} {}:{} couldnt open iio context ",
+                    Utc::now(),
+                    file!(),
+                    line!()
+                );
+            }
+            std::process::exit(1);
+        });
+        let mut trigs = Vec::new();
+        for dev in ctx.devices() {
+            if dev.is_trigger() {
+                match dev.id() {
+                    Some(id) => trigs.push(id),
+                    None => (),
+                }
+            } else {
+                println!(
+                    "{} [{}]: {} channels",
+                    dev.id().unwrap_or_default(),
+                    dev.name().unwrap_or_default(),
+                    dev.num_channels()
+                );
+            }
+        }
+        if trigs.is_empty() {
+            {
+                println!("{} {}:{} no triggers ", Utc::now(), file!(), line!());
+            }
+        } else {
+            for s in trigs {
+                println!("trigger {}", s);
+            }
+        };
+        let dev = ctx.find_device("cf-ad9361-lpc").unwrap_or_else(|| {
+            {
+                println!(
+                    "{} {}:{} no device named cf-ad9361-lpc ",
+                    Utc::now(),
+                    file!(),
+                    line!()
+                );
+            }
+            std::process::exit(2);
+        });
+        let phy = ctx.find_device("ad9361-phy").unwrap_or_else(|| {
+            {
+                println!(
+                    "{} {}:{} no device named ad9361-phy ",
+                    Utc::now(),
+                    file!(),
+                    line!()
+                );
+            }
+            std::process::exit(2);
+        });
+        let mut nchan = 0;
+        for mut chan in dev.channels() {
+            if (Some(std::any::TypeId::of::<i16>())) == (chan.type_of()) {
+                nchan += 1;
+                chan.enable();
+            };
+        }
+        if (0) == (nchan) {
+            {
+                println!(
+                    "{} {}:{} no 16 bit channels found ",
+                    Utc::now(),
+                    file!(),
+                    line!()
+                );
+            }
+            std::process::exit(1);
+        } else {
+            {
+                println!(
+                    "{} {}:{} 16 bit channels found  nchan={}",
+                    Utc::now(),
+                    file!(),
+                    line!(),
+                    nchan
+                );
+            }
+        };
+        let (s, r) = crossbeam_channel::bounded(3);
+        let mut chans = Vec::new();
+        let mut buf = dev.create_buffer(512, false).unwrap_or_else(|err| {
+            {
+                println!(
+                    "{} {}:{} can't create buffer  err={}",
+                    Utc::now(),
+                    file!(),
+                    line!(),
+                    err
+                );
+            }
+            std::process::exit(3);
+        });
+        for ch in dev.channels() {
+            chans.push(ch);
+        }
+        crossbeam_utils::thread::scope(|scope| {
+            scope.spawn(|_| {
+                {
+                    println!("{} {}:{} start fftw plan ", Utc::now(), file!(), line!());
+                }
+                let mut plan: fftw::plan::C2CPlan64 = fftw::plan::C2CPlan::aligned(
+                    &[512],
+                    fftw::types::Sign::Forward,
+                    fftw::types::Flag::Measure,
+                )
+                .unwrap();
+                {
+                    println!("{} {}:{} finish fftw plan ", Utc::now(), file!(), line!());
+                }
+                loop {
+                    let tup: usize = r.recv().ok().unwrap();
+                    let mut hfftin = fftin.clone();
+                    let mut lfftin = hfftin.lock().unwrap();
+                    let mut ha = lfftin[tup].clone();
+                    let mut a = &mut ha.lock().unwrap();
+                    let mut hfftout = fftout.clone();
+                    let mut lfftout = hfftout.lock().unwrap();
+                    let mut hb = lfftout[tup].clone();
+                    let mut b = &mut hb.lock().unwrap();
+                    plan.c2c(&mut a.ptr, &mut b.ptr).unwrap();
+                    b.timestamp = Utc::now();
+                    {
+                        println!(
+                            "{} {}:{}   tup={}  (b.timestamp-a.timestamp)={}  b.ptr[0]={}",
+                            Utc::now(),
+                            file!(),
+                            line!(),
+                            tup,
+                            (b.timestamp - a.timestamp),
+                            b.ptr[0]
+                        );
+                    }
+                    send_to_fft_scaler.send(tup).unwrap();
+                }
+            });
+            let mut count = 0;
+            loop {
+                match buf.refill() {
+                    Err(err) => {
+                        {
+                            println!(
+                                "{} {}:{} error filling buffer  err={}",
+                                Utc::now(),
+                                file!(),
+                                line!(),
+                                err
+                            );
+                        }
+                        std::process::exit(4)
+                    }
+                    _ => (),
+                }
+                {
+                    let time_acquisition = Utc::now();
+                    let mut hfftin = fftin.clone();
+                    let mut lfftin = hfftin.lock().unwrap();
+                    let mut ha = lfftin[count].clone();
+                    let mut a = &mut ha.lock().unwrap();
+                    let data_i: Vec<i16> = buf.channel_iter::<i16>(&(chans[0])).collect();
+                    let data_q: Vec<i16> = buf.channel_iter::<i16>(&(chans[1])).collect();
+                    a.timestamp = time_acquisition;
+                    for i in 0..512 {
+                        a.ptr[i] = fftw::types::c64::new((data_i[i] as f64), (data_q[i] as f64));
+                    }
+                }
+                {
+                    println!(
+                        "{} {}:{} sender  count={}",
+                        Utc::now(),
+                        file!(),
+                        line!(),
+                        count
+                    );
+                }
+                s.send(count).unwrap();
+                count += 1;
+                if (3) <= (count) {
+                    count = 0;
+                };
+            }
+        })
+        .unwrap();
+    });
     let (send_to_fft_scaler, recv_at_fft_scaler) = crossbeam_channel::bounded(3);
     let count: usize = 1;
     send_to_fft_scaler.send(count).unwrap();
     iio_reader::iio_read(fftin, fftout, send_to_fft_scaler);
-    fft_scaler::fft_scaler(fftout, recv_at_fft_scaler);
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     let (mut window, events) = glfw
--- gen00.lisp
+++ gen00.lisp
@@ -92,37 +92,225 @@ panic = \"abort\"
 	   "extern crate fftw;"
 	   
 
-	   "mod iio_reader;"
-	   "mod fft_scaler;"
+
 	   (use (imgui_glfw_rs glfw (curly Action Context Key))
 		;(imgui_glfw_rs imgui)
 		(imgui_glfw_rs ImguiGLFW)
 		(std os raw c_void)
 		(std ffi CString)
 		(iio_reader SendComplex)
-		;(fftw)
+					
 		)
+
+	   (use (std thread spawn)
+		(std sync (curly Arc Mutex))
+		(std io)
+		(crossbeam_channel bounded))
+	   
+	   (use (fftw)
+		)
+	   
+	   (use 
+		(fftw plan C2CPlan)
+		)
+
+	   (do0
+	    (space pub
+	     (defstruct0 SendComplex
+		 ("pub timestamp" "DateTime<Utc>")
+				   
+	       ("pub ptr"
+		"fftw::array::AlignedVec<num_complex::Complex<f64>>"
+					;"*mut num_complex::Complex<f64>"
+		)))
+				
+	    "unsafe impl Send for SendComplex {}")
+
+
+
+	   
 	       
 	   (defun main ()
+	     (let (((values s0 r0) (crossbeam_channel--bounded ,n-buf))
+		   ((values s1 r1) (crossbeam_channel--bounded ,n-buf)))
+	      (let* (
+		     (fftin
 
-	     (let* (
-		    (fftin
+		      (list ,@(loop for i below n-buf collect
+				   `(std--sync--Arc--new
+				     (std--sync--Mutex--new
+				      (make-instance SendComplex :timestamp (Utc--now) :ptr (fftw--array--AlignedVec--new ,n-samples))
+				      )))))
+		     (fftout
+		      (list ,@(loop for i below n-buf collect
+				   `(std--sync--Arc--new
+				     (std--sync--Mutex--new
+				      (make-instance SendComplex :timestamp (Utc--now) :ptr (fftw--array--AlignedVec--new ,n-samples))
+				      )))))
 
-		     (std--sync--Arc--new
-		      (std--sync--Mutex--new
-		       (list ,@(loop for i below n-buf collect
-				    `(std--sync--Arc--new
-				      (std--sync--Mutex--new
-				       (make-instance SendComplex :timestamp (Utc--now) :ptr (fftw--array--AlignedVec--new ,n-samples))
-				       )))))))
-		    (fftout
-		     (std--sync--Arc--new
-		      (std--sync--Mutex--new
-		       (list ,@(loop for i below n-buf collect
-				    `(std--sync--Arc--new
-				      (std--sync--Mutex--new
-				       (make-instance SendComplex :timestamp (Utc--now) :ptr (fftw--array--AlignedVec--new ,n-samples))
-				       )))))))))
+		    
+		     )
+
+		(let ((core_ids (dot (core_affinity--get_core_ids)
+				     (unwrap))))
+		  #+nil (for (a core_ids)
+			     ,(logprint "affinity" `(a))))
+		(let ((b (dot (std--thread--Builder--new)
+			      (name (dot (string "pluto_reader")
+					 (into)))))
+		      (reader_thread
+		       (b.spawn
+			(space
+			 move
+			 (lambda ()
+			   (do0
+			    (core_affinity--set_for_current (make-instance core_affinity--CoreId :id 0))
+			    ;; https://github.com/fpagliughi/rust-industrial-io/blob/master/examples/riio_detect.rs
+			    (let ((ctx (dot (iio--Context--create_network (string "192.168.2.1"))
+					    (unwrap_or_else (lambda (err_)
+							      ,(logprint "couldnt open iio context")
+							      (std--process--exit 1))))))
+			      (let* ((trigs (Vec--new)))
+				(for (dev (ctx.devices))
+				     (if (dev.is_trigger)
+					 (case (dev.id)
+					   ((Some id) (trigs.push id))
+					   (None "()"))
+					 (println! (string "{} [{}]: {} channels")
+						   (dot dev
+							(id)
+							(unwrap_or_default))
+						   (dot dev
+							(name)
+							(unwrap_or_default))
+						   (dot dev
+							(num_channels)))))
+			       
+				(if (trigs.is_empty)
+				    ,(logprint "no triggers" `())
+				    (for (s trigs)
+					 (println! (string "trigger {}")
+						   s))))
+
+			      (let (,@ (loop for (var name) in `((dev cf-ad9361-lpc)
+								 (phy ad9361-phy))
+					  collect
+					    `(,var (dot ctx
+							(find_device (string ,name))
+							(unwrap_or_else
+							 (lambda ()
+							   ,(logprint (format nil "no device named ~a" name) `())
+							   (std--process--exit 2))))))
+				    ) 
+
+				(let* ((nchan 0))
+				  (for ("mut chan" (dev.channels))
+				       (when (== (Some (std--any--TypeId--of--<i16>))
+						 (chan.type_of))
+					 (incf nchan)
+					 (chan.enable)))
+				  (if (== 0 nchan)
+				      (do0
+				       ,(logprint "no 16 bit channels found" `())
+				       (std--process--exit 1))
+				      ,(logprint "16 bit channels found" `(nchan))))
+				(do0
+				
+				
+				 (let* ((chans (Vec--new)))
+				   (let* ((buf (dot dev
+						    ;; cyclic buffer only makes sense for output (to repeat waveform)
+						    (create_buffer ,n-samples false)
+						    (unwrap_or_else (lambda (err)
+								      ,(logprint (format nil "can't create buffer") `(err))
+								      (std--process--exit 3))))))
+				     (do0 
+				      (for (ch (dev.channels))
+					   (chans.push ch))
+				      (dot (crossbeam_utils--thread--scope
+					    (lambda (scope)
+					      (scope.spawn (lambda (_)
+							     ,(logprint "start fftw plan" `())
+							     (let* ((plan (dot (fftw--plan--C2CPlan--aligned ,(format nil "&[~a]" n-samples)
+													     fftw--types--Sign--Forward
+													     fftw--types--Flag--Measure)
+									       (unwrap))))
+							       (declare (type fftw--plan--C2CPlan64 plan))
+							       ,(logprint "finish fftw plan" `())
+							       (loop
+								  (let ((tup (dot r
+										  (recv)
+										  (ok)
+										  (unwrap))))
+								    (declare (type usize tup))
+								    (let* (
+									   (ha (dot (aref fftin tup)
+										    (clone)
+										    ))
+									   (a (space "&mut" (dot ha
+												 (lock)
+												 (unwrap)
+												 )))
+									   
+									   (hb (dot (aref fftout tup)
+										    (clone)))
+									   (b (space "&mut" (dot hb
+												 (lock)
+												 (unwrap)))))
+								      (do0
+								       (dot plan
+									    (c2c "&mut a.ptr" "&mut b.ptr")
+									    (unwrap))
+								       (setf b.timestamp (Utc--now)))
+								      ,(logprint "" `(tup (- b.timestamp
+											     a.timestamp)
+											  (aref b.ptr 0)))
+								      (dot send_to_fft_scaler
+									   (send tup)
+									   (unwrap))))))))
+					      (let* ((count 0))
+						(loop
+						   (case (buf.refill)
+						     ((Err err)
+						      ,(logprint "error filling buffer" `(err))
+						      (std--process--exit 4))
+						     (t "()"))
+						   ;; https://users.rust-lang.org/t/solved-how-to-move-non-send-between-threads-or-an-alternative/19928
+						   (progn
+						     (let ((time_acquisition (Utc--now)))
+						       (let* ((hfftin (dot fftin (clone)))
+							      (lfftin (dot hfftin (lock) (unwrap)))
+
+							      (ha (dot (aref lfftin count)
+								       (clone)))
+							      (a (space "&mut" (dot ha
+										    (lock)
+										    (unwrap)))))
+							 (let ((data_i (dot buf
+									    (channel_iter--<i16> (ref (aref chans 0)))
+									    (collect)))
+							       (data_q (dot buf
+									    (channel_iter--<i16> (ref (aref chans 1)))
+									    (collect))))
+							   (declare (type Vec<i16> data_i data_q))
+							   (do0
+							    (setf a.timestamp time_acquisition)
+							    (for (i (slice 0 ,n-samples))
+								 (setf (aref a.ptr i) (fftw--types--c64--new (coerce (aref data_i i)
+														     f64)
+													     (coerce (aref data_q i)
+														     f64)))))))))
+						   ,(logprint "sender" `(count ))
+						   (dot s
+							(send count)
+							(unwrap))
+						   (incf count)
+						   (when (<= ,n-buf count)
+						     (setf count 0))))))
+					   (unwrap)))))))))))))))
+
+
+		))
 	     
 	     
 	     
@@ -132,11 +320,11 @@ panic = \"abort\"
 					;(iio_reader--iio_read send_to_fft_scaler)
 	       (let ((count 1))
 		 (declare (type usize count))
-		(dot send_to_fft_scaler
-		     (send count)
-		     (unwrap)))
+		 (dot send_to_fft_scaler
+		      (send count)
+		      (unwrap)))
 	       (iio_reader--iio_read fftin fftout send_to_fft_scaler)
-	       (fft_scaler--fft_scaler fftout recv_at_fft_scaler)
+					;(fft_scaler--fft_scaler fftout recv_at_fft_scaler)
 	       )
 	     
 	     (let* ((glfw (dot (glfw--init glfw--FAIL_ON_ERRORS)
@@ -212,7 +400,7 @@ panic = \"abort\"
 					    (return (dot  window
 							  (get_proc_address symbol))
 						    )
-				     
+					    
 					    )))))
 		   (while (not (window.should_close))
 		     (space unsafe
@@ -235,7 +423,7 @@ panic = \"abort\"
 					    (build))
 				       (ui.text (string "bla3")))))
 					;(ui.text (string "bla"))
-			
+			 
 			 (ui.show_demo_window "&mut true")
 			 (imgui_glfw.draw ui "&mut window")))
 		     
@@ -258,7 +446,7 @@ panic = \"abort\"
 			     (window.set_should_close true))
 			    (t "{}")))))))))))
 
-    (define-module
+    #+nil (define-module
 	`(iio_reader
 	  (do0
 
@@ -274,204 +462,10 @@ panic = \"abort\"
 		(fftw plan C2CPlan)
 		)
 
-	   (do0
-	    (space pub
-	     (defstruct0 SendComplex
-		 ("pub timestamp" "DateTime<Utc>")
-				   
-	       ("pub ptr"
-		"fftw::array::AlignedVec<num_complex::Complex<f64>>"
-					;"*mut num_complex::Complex<f64>"
-		)))
-				
-				"unsafe impl Send for SendComplex {}")
 	   
-	   (space pub
-
-		  (defun iio_read ("fftin : Arc<Mutex<[Arc<Mutex<SendComplex>>;3]>>"
-				   "fftout : Arc<Mutex<[Arc<Mutex<SendComplex>>;3]>>"
-				   "send_to_fft_scaler : crossbeam_channel::Sender<usize>"
-				   )
-	      (let ((core_ids (dot (core_affinity--get_core_ids)
-				   (unwrap))))
-		#+nil (for (a core_ids)
-		     ,(logprint "affinity" `(a))))
-	      (let ((b (dot (std--thread--Builder--new)
-			    (name (dot (string "pluto_reader")
-				       (into)))))
-		    (reader_thread
-		     (b.spawn
-		      (space
-		       move
-		       (lambda ()
-			 (do0
-			  (core_affinity--set_for_current (make-instance core_affinity--CoreId :id 0))
-			  ;; https://github.com/fpagliughi/rust-industrial-io/blob/master/examples/riio_detect.rs
-			  (let ((ctx (dot (iio--Context--create_network (string "192.168.2.1"))
-					  (unwrap_or_else (lambda (err_)
-							    ,(logprint "couldnt open iio context")
-							    (std--process--exit 1))))))
-			    (let* ((trigs (Vec--new)))
-			      (for (dev (ctx.devices))
-				   (if (dev.is_trigger)
-				       (case (dev.id)
-					 ((Some id) (trigs.push id))
-					 (None "()"))
-				       (println! (string "{} [{}]: {} channels")
-						 (dot dev
-						      (id)
-						      (unwrap_or_default))
-						 (dot dev
-						      (name)
-						      (unwrap_or_default))
-						 (dot dev
-						      (num_channels)))))
-			     
-			      (if (trigs.is_empty)
-				  ,(logprint "no triggers" `())
-				  (for (s trigs)
-				       (println! (string "trigger {}")
-						 s))))
-
-			    (let (,@ (loop for (var name) in `((dev cf-ad9361-lpc)
-							       (phy ad9361-phy))
-					collect
-					  `(,var (dot ctx
-						      (find_device (string ,name))
-						      (unwrap_or_else
-						       (lambda ()
-							 ,(logprint (format nil "no device named ~a" name) `())
-							 (std--process--exit 2))))))
-				  ) 
-
-			      (let* ((nchan 0))
-				(for ("mut chan" (dev.channels))
-				     (when (== (Some (std--any--TypeId--of--<i16>))
-					       (chan.type_of))
-				       (incf nchan)
-				       (chan.enable)))
-				(if (== 0 nchan)
-				    (do0
-				     ,(logprint "no 16 bit channels found" `())
-				     (std--process--exit 1))
-				    ,(logprint "16 bit channels found" `(nchan))))
-
-			      ;; https://users.rust-lang.org/t/sharing-buffer-between-threads-without-locking/10508
-			      ;; https://docs.rs/triple_buffer/5.0.4/triple_buffer/
-			      ;; https://medium.com/@polyglot_factotum/rust-concurrency-patterns-communicate-by-sharing-your-sender-11a496ce7791
-			      ;; https://wiki.analog.com/resources/tools-software/linux-software/libiio_internals
-			      ;; 2017-03 https://users.rust-lang.org/t/spmc-buffer-triple-buffering-for-multiple-consumers/10118
-			      ;; 2017-11 https://users.rust-lang.org/t/code-review-triplebuffer-for-sending-huge-objects-between-threads/13787/7
-			      ;; https://github.com/HadrienG2/triple-buffer consumer is not in sync with producer
-			      ;; https://doc.rust-lang.org/book/ch16-02-message-passing.html
-			      ;; https://stjepang.github.io/2019/01/29/lock-free-rust-crossbeam-in-2019.html scoped thread, atomic cell
-			      (do0
-			       ;; https://users.rust-lang.org/t/how-can-i-allocate-aligned-memory-in-rust/33293 std::slice::from_raw_parts[_mut]
-			       
-				 (let (((values s r) (crossbeam_channel--bounded 3))
-				       )
-
-				   (let* 				       (
-										
-										(chans (Vec--new))
-					;(count 0)
-										)
-
-				     (let* ((buf (dot dev
-			      ;; cyclic buffer only makes sense for output (to repeat waveform)
-			      (create_buffer ,n-samples false)
-			      (unwrap_or_else (lambda (err)
-						,(logprint (format nil "can't create buffer") `(err))
-						(std--process--exit 3))))))
-				       (do0 
-					(for (ch (dev.channels))
-					     (chans.push ch))
-					(dot (crossbeam_utils--thread--scope
-					      (lambda (scope)
-						(scope.spawn (lambda (_)
-							       ,(logprint "start fftw plan" `())
-							       (let* ((plan (dot (fftw--plan--C2CPlan--aligned ,(format nil "&[~a]" n-samples)
-													       fftw--types--Sign--Forward
-													       fftw--types--Flag--Measure)
-										 (unwrap))))
-								 (declare (type fftw--plan--C2CPlan64 plan))
-								 ,(logprint "finish fftw plan" `())
-								 (loop
-								    (let ((tup (dot r
-										    (recv)
-										    (ok)
-										    (unwrap))))
-								      (declare (type usize tup))
-								      (let* ((hfftin (dot fftin (clone)))
-									     (lfftin (dot hfftin (lock) (unwrap)))
-
-									     (ha (dot (aref lfftin tup)
-										      (clone)
-										      ))
-									     (a (space "&mut" (dot ha
-												   (lock)
-												   (unwrap)
-												   )))
-									     (hfftout (dot fftout (clone)))
-									     (lfftout (dot hfftout (lock) (unwrap)))
-
-									     (hb (dot (aref lfftout tup)
-										      (clone)))
-									     (b (space "&mut" (dot hb
-												   (lock)
-												   (unwrap)))))
-									(do0
-									 (dot plan
-									      (c2c "&mut a.ptr" "&mut b.ptr")
-									      (unwrap))
-									 (setf b.timestamp (Utc--now)))
-									,(logprint "" `(tup (- b.timestamp
-											       a.timestamp)
-											    (aref b.ptr 0)))
-									(dot send_to_fft_scaler
-									     (send tup)
-									     (unwrap))))))))
-						(let* ((count 0))
-						  (loop
-						     (case (buf.refill)
-						       ((Err err)
-							,(logprint "error filling buffer" `(err))
-							(std--process--exit 4))
-						       (t "()"))
-						     ;; https://users.rust-lang.org/t/solved-how-to-move-non-send-between-threads-or-an-alternative/19928
-						     (progn
-						       (let ((time_acquisition (Utc--now)))
-							 (let* ((hfftin (dot fftin (clone)))
-								(lfftin (dot hfftin (lock) (unwrap)))
-
-								(ha (dot (aref lfftin count)
-									 (clone)))
-								(a (space "&mut" (dot ha
-										      (lock)
-										      (unwrap)))))
-							   (let ((data_i (dot buf
-									      (channel_iter--<i16> (ref (aref chans 0)))
-									      (collect)))
-								 (data_q (dot buf
-									      (channel_iter--<i16> (ref (aref chans 1)))
-									      (collect))))
-							     (declare (type Vec<i16> data_i data_q))
-							     (do0
-							      (setf a.timestamp time_acquisition)
-							      (for (i (slice 0 ,n-samples))
-								   (setf (aref a.ptr i) (fftw--types--c64--new (coerce (aref data_i i)
-														       f64)
-													       (coerce (aref data_q i)
-														       f64)))))))))
-						     ,(logprint "sender" `(count ))
-						     (dot s
-							  (send count)
-							  (unwrap))
-						     (incf count)
-						     (when (<= ,n-buf count)
-						       (setf count 0))))))
-					     (unwrap)))))))))))))))))))))
-    (define-module
+	   
+	   )))
+    #+nil (define-module
 	`(fft_scaler
 	  (do0
 
